730e632cc7cefccb8334b5b99a6eb0860c6b039d
airmick <32387739+airmick@users.noreply.github.com>
2021-05-31 12:37:03 +0200

    Configure Loaders to fetch data

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,57 @@
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EarthquakeLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    /**
+     * Subclasses must implement this to take care of loading their data,
+     * as per {@link #startLoading()}.  This is not called by clients directly,
+     * but as a result of a call to {@link #startLoading()}.
+     */
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * Called on a worker thread to perform the actual load and to return
+     * the result of the load operation.
+     */
+    @Override
+    public List<Earthquake> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+
+
+}
